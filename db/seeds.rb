da01784c9c68c8b159409b896c2d882456987d08
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 19:41:23 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-User.find_by(email: 'kyaroru@yandex.com')&.update(admin: true)
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.find_by(email: 'kyaroru@yandex.com')
+if user
+  user.update_columns(admin: true) # update_columns skips validations
+  puts "Successfully made #{user.email} an admin"
+else
+  puts "User not found!"
+end
