67b726abbed0ea153fcbe9fc8d243e9a0d410761
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 20:06:24 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-
+user = User.find_by(email: 'kyaroru@yandex.com')
+if user
+  user.update_columns(admin: true) # update_columns skips validations
+  puts "Successfully made #{user.email} an admin"
+else
+  puts "User not found!"
+end
