b9ac65c73e8da1a0b7edb8bef6b2a81182160a26
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 15:53:43 +0100

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # This sends the email right after the user is saved to the database
+      # Upewnij się, że masz UserMailer zdefiniowany w app/mailers/user_mailer.rb
       UserMailer.welcome_email(@user).deliver_later
       redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    # Dodano brakującą metodę parametrów
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
