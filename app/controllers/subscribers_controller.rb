bd92e05b3c2a18c70cf1ef93bd9d507802d604ec
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 10:06:29 +0100

    Update subscribers_controller.rb

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,7 +1,21 @@
-class NotificationMailer < ApplicationMailer
-  def welcome_email(user_email)
-    @user_email = user_email
-    @url = "http://localhost:3000"
-    mail(to: @user_email, subject: "Welcome to The Kudo Archive")
+class SubscribersController < ApplicationController
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+
+    if @subscriber.save
+      # Wysyłka maila powitalnego przy użyciu NotificationMailer
+      # Używamy deliver_later, aby nie spowalniać ładowania strony
+      NotificationMailer.welcome_email(@subscriber.email).deliver_later
+      
+      redirect_to root_path, notice: "Dziękujemy za zapisanie się do newslettera!"
+    else
+      redirect_to root_path, alert: "Nie udało się zapisać: #{@subscriber.errors.full_messages.to_sentence}"
+    end
+  end
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
   end
 end
