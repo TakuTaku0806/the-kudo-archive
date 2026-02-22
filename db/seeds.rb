bcf8b0314a1f55780c67f90fc41701424ead9aa4
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 20:08:23 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+# Znajdź konto, które przed chwilą stworzyłeś
 user = User.find_by(email: 'kyaroru@yandex.com')
+
 if user
-  user.update_columns(admin: true) # update_columns skips validations
-  puts "Successfully made #{user.email} an admin"
+  user.update_columns(admin: true)
+  puts "AUTORYZACJA: Użytkownik #{user.email} został mianowany administratorem."
 else
-  puts "User not found!"
+  puts "BŁĄD: Nie znaleziono użytkownika o tym mailu. Sprawdź literówki!"
+  puts "Lista kont w bazie: #{User.pluck(:email)}"
 end
