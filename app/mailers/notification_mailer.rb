4198d84867df0a739781250892af442c1521e9f4
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 15:59:04 +0100

    Update notification_mailer.rb

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,8 @@
 class NotificationMailer < ApplicationMailer
-  # Możesz tu ustawić domyślny adres "od kogo"
   default from: 'notifications@the-kudo-archive.onrender.com'
 
   def welcome_email(user_email)
     @user_email = user_email
-    # Zmienione na Twój adres na Renderze
     @url = "https://the-kudo-archive.onrender.com" 
     
     mail(to: @user_email, subject: "Welcome to The Kudo Archive")
