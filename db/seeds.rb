b3b890efb94f992b03263ab9c919d85a94b6d7e8
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 20:00:15 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-user = User.find_by(email: 'kyaroru@yandex.com')
-if user
-  user.destroy
-  puts "SUCCESS: User #{user.email} has been deleted."
-else
-  puts "NOTICE: User not found. They might already be gone."
-end
+
