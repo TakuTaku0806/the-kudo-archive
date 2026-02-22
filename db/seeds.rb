0b4eaab41b9d7a60099fd6b5b8956056527902b8
TakuTaku0806 <kyaroru@yandex.com>
2026-02-22 19:33:10 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+User.find_by(email: 'kyaroru@yandex.com')&.update(admin: true)
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
